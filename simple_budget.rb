3a42da827e0bef2108f9835d8d6ead8d516fb91d
Matt Lankford <lankford@mattlankford.com>
2017-10-24 01:18:58 -0400

    Now that the message is in the Group class... the FLOW of the program is starting to become obvious... THIS is what we have been trying to get to... THIS is why we REFACTOR... so that we can easily understand what is happening

--- simple_budget.rb
+++ simple_budget.rb
@@ -63,6 +63,26 @@ class Group
     members.reduce([]) { |incomes,user| incomes << "\n#{user.name} makes $#{sprintf('%.2f', user.monthly_income)} per month, and $#{sprintf('%.2f', user.annual_income)} per year." }
   end
 
+  def output_message
+    msg = <<~MSG
+
+    Combined, this household makes $#{sprintf('%.2f', combined_monthly)} per month, and $#{sprintf('%.2f', combined_annual)} per year.
+
+    Here is a break down of your monthly budget:
+      * You can afford a living expense of $#{sprintf('%.2f', living)}.
+      * You can afford $#{sprintf('%.2f', bills)} in bills.
+      * You can afford $#{sprintf('%.2f', gas)} in gas.
+      * You can afford $#{sprintf('%.2f', groceries)} in groceries and food.
+
+    This leaves you with roughly $#{sprintf('%.2f', leftover)} left over
+    for spending and saving.
+    Keep in mind that these estimates are based on averages,
+    and provide room for flexibility.
+    Also remember to try and stay under budget so that
+    you have more left over each month.
+    MSG
+  end
+
 end
 
 clear_the_screen
@@ -73,12 +93,6 @@ members            = group.members
 combined_monthly   = group.combined_monthly
 combined_annual    = group.combined_annual
 
-living             = group.living
-bills              = group.bills
-gas                = group.gas
-groceries          = group.groceries
-leftover           = group.leftover
-
 individual_incomes = group.individual_incomes
 
 file_name          = get_input "\n\nWhat would you like to name your budget? "
@@ -86,23 +100,7 @@ file_name          = "#{file_name}.txt"
 
 output_file        = open(file_name, 'w')
 output_file.write individual_incomes.join
-output_file.write msg = <<MSG
-
-Combined, this household makes $#{sprintf('%.2f', combined_monthly)} per month, and $#{sprintf('%.2f', combined_annual)} per year.
-
-Here is a break down of your monthly budget:
-  * You can afford a living expense of $#{sprintf('%.2f', living)}.
-  * You can afford $#{sprintf('%.2f', bills)} in bills.
-  * You can afford $#{sprintf('%.2f', gas)} in gas.
-  * You can afford $#{sprintf('%.2f', groceries)} in groceries and food.
-
-This leaves you with roughly $#{sprintf('%.2f', leftover)} left over
-for spending and saving.
-Keep in mind that these estimates are based on averages,
-and provide room for flexibility.
-Also remember to try and stay under budget so that
-you have more left over each month.
-MSG
+output_file.write group.output_message
 
 clear_the_screen
 
