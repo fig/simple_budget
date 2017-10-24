a3dc48e3897c64c90015f7322df1a075242c9183
Matt Lankford <lankford@mattlankford.com>
2017-10-24 02:03:24 -0400

    So sprintf works, but ruby has a shortcut for it... and let's move that join to a shorter location

--- groups.rb
+++ groups.rb
@@ -43,25 +43,25 @@ class Group
   end
 
   def individual_incomes
-    members.reduce([]) { |incomes,user| incomes << "\n#{user.name} makes $#{sprintf('%.2f', user.monthly_income)} per month, and $#{sprintf('%.2f', user.annual_income)} per year." } .join
+    members.reduce([]) { |incomes,user| incomes << "\n#{user.name} makes $#{'%.2f' % user.monthly_income} per month, and $#{'%.2f' % user.annual_income} per year." }
   end
 
   def output_message
-    individual_incomes + aggregate_output_message
+    individual_incomes.join + aggregate_output_message
   end
 
   def aggregate_output_message
     msg = <<~MSG
 
-    Combined, this household makes $#{sprintf('%.2f', combined_monthly)} per month, and $#{sprintf('%.2f', combined_annual)} per year.
+    Combined, this household makes $#{'%.2f' % combined_monthly} per month, and $#{'%.2f' % combined_annual} per year.
 
     Here is a break down of your monthly budget:
-      * You can afford a living expense of $#{sprintf('%.2f', living)}.
-      * You can afford $#{sprintf('%.2f', bills)} in bills.
-      * You can afford $#{sprintf('%.2f', gas)} in gas.
-      * You can afford $#{sprintf('%.2f', groceries)} in groceries and food.
+      * You can afford a living expense of $#{'%.2f' % living}.
+      * You can afford $#{'%.2f' % bills} in bills.
+      * You can afford $#{'%.2f' % gas} in gas.
+      * You can afford $#{'%.2f' % groceries} in groceries and food.
 
-    This leaves you with roughly $#{sprintf('%.2f', leftover)} left over
+    This leaves you with roughly $#{'%.2f' % leftover} left over
     for spending and saving.
     Keep in mind that these estimates are based on averages,
     and provide room for flexibility.
